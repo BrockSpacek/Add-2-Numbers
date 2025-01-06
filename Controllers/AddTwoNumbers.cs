6b9f5fa21bf1f53d735d81811cff05ee20cfb475
BrockSpacek <brockspacek1@gmail.com>
2025-01-06 15:59:08 -0800

    Finished

--- Controllers/AddTwoNumbers.cs
+++ Controllers/AddTwoNumbers.cs
@@ -12,7 +12,7 @@ namespace Add_2_Numbers.Controllers
 
         public string addTwoNumbers(int num1, int num2)
         {
-            return $"{num1} + {num2} = {num1 + num2}.";
+            return $"If you add {num1} + {num2}, it will equal {num1 + num2}.";
     }
     }
 }
